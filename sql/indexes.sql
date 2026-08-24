6ddafae2a84cabe21a33da89b99056953f4864a3
nibblerin <samoilovichnastya@gmail.com>
2026-08-24 14:32:51 +0200

    added some new indexes

--- sql/indexes.sql
+++ sql/indexes.sql
@@ -1,2 +1,17 @@
-CREATE INDEX idx_students_room_id
+CREATE INDEX IF NOT EXISTS idx_students_room_id
 ON students (room_id);
+
+CREATE INDEX IF NOT EXISTS idx_students_birthday
+ON students (birthday);
+
+CREATE INDEX IF NOT EXISTS idx_students_room_birthday
+ON students (room_id, birthday);
+
+CREATE INDEX IF NOT EXISTS idx_students_room_sex
+ON students (room_id, sex);
+
+CREATE INDEX IF NOT EXISTS idx_students_name
+ON students (name);
+
+CREATE INDEX IF NOT EXISTS idx_rooms_name
+ON rooms (name);
